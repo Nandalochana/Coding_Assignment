a83d4a089e6507e04f4567c2a6a1640c158f3992
NandaLochana <danushkanandalochana@gmail.com>
2024-06-02 22:57:58 +0300

    Safety retry mechanism implemented and changes committed

--- assignment/src/main/java/com/example/assignment/services/CountryService.java
+++ assignment/src/main/java/com/example/assignment/services/CountryService.java
@@ -22,31 +22,83 @@ public class CountryService implements CountryServiceProviders {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    /**
+     * Load all country list
+     * @return Flux<Country>
+     */
     @Override
     public Flux<Country> getAllCountries() {
         return Mono.fromCallable(() -> {
+            Country[] countries = null;
             String url = apiUrl + "/all";
-            log.info("Fetch Url :" + url);
-            Country[] countries = restTemplate.getForObject(url.trim(), Country[].class);
-            log.info("Found Result :" + (countries != null ? countries.length : "0"));
+            try {
+                log.info("Fetch Url :" + url);
+                countries = getCountryInfo(url);
+                log.info("Found Result :" + (countries != null ? countries.length : "0"));
+            }
+            catch (Exception exception) {
+                log.info("Exception occurred :" + exception.getMessage());
+                countries = getCountryInfo(url);
+            }
             return countries != null ? Arrays.asList(countries) : new ArrayList<Country>();
         }).flatMapMany(Flux::fromIterable);
     }
 
+
+    /***
+     * Load Specific country information according to given name
+     * @param name String (Country name)
+     * @return Mono<Country>
+     */
     @Override
     public Mono<Country> getCountryByName(String name) {
-        String url = apiUrl + "/name/" + name;
-        log.info("Fetch Url :" + url);
         return Mono.fromCallable(() -> {
-            Country[] countries = restTemplate.getForObject(url.trim(), Country[].class);
-            if (countries != null && countries.length > 0) {
-                Country country = countries[0];
-                log.info("Found Result :" + country.toString());
-                return country;
-            } else {
-                log.info("No Result Found");
-                return null;
+            String url = apiUrl + "/name/" + name;
+            log.info("Fetch Url :" + url);
+            try {
+                return getCountry(url);
+            }
+            catch (Exception exception) {
+                log.info("Exception occurred :" + exception.getMessage());
+                return getCountry(url);
             }
         }).flatMap(Mono::justOrEmpty);
+    }
+
+    /***
+     * Load specific country information
+     * Note: In this part it will validate the return information
+     * @param url String (url)
+     * @return Country
+     */
+    private Country getCountry(String url) {
+        Country[]  countries = getCountryInfo(url);
+        if (countries != null && countries.length > 0) {
+            Country country = countries[0];
+            log.info("Found Result :" + country.toString());
+            return country;
+        }
+        else {
+            log.info("No Result Found");
+            return null;
+        }
+    }
+
+    /***
+     * In this part it will recursively call until the result found
+     * Note: Sometime the country API is not working that why this kind of API is implemented
+     * @param url String (url)
+     * @return Country[]
+     */
+    private Country[] getCountryInfo(String url) {
+        try {
+            log.info("Loading Information : (URL : " + url + ")");
+            return restTemplate.getForObject(url.trim(), Country[].class);
+        }
+        catch (Exception exception) {
+            log.info("Exception occurred (Called Again):" + exception.getMessage());
+            return getCountryInfo(url);
+        }
+
     }
 }
